2a48d2c2e2b8cad7e73b2b38254da0cc3f951b42
Intelligence-Tests <intelligencetests@gmail.com>
2024-10-17 12:04:38 +0530

    JB

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import store from './redux/store.js'
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { Helmet } from "react-helmet";
 const { VITE_CLERK_PUBLISHABLE_KEY } = import.meta.env
 const publishableKey = VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -19,6 +20,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
+          <Helmet>
+            <meta name="monetag" content="dbfaa86c271d520fdbb2a456805fb3de" />
+          </Helmet>          
           <Toaster />
         </PersistGate>
       </Provider>
